bf858e31c0904bf4e3f9f1860bc191ed24d7278a
Basel <baseline7@live.com>
2023-06-20 10:47:32 -0700

    Initial commit

---
+++ src/main/java/com/carector/App.java
@@ -0,0 +1,13 @@
+package com.carector;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/carector/StringCalculatorTest.java
@@ -0,0 +1,14 @@
+package com.carector;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class StringCalculatorTest 
+{
+    
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
