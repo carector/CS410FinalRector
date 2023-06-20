e1605339270abcbe924d75e1a3f6310b42ffd7fc
Basel <baseline7@live.com>
2023-06-20 10:58:02 -0700

    Add written document, update test cases

--- src/test/java/com/carector/StringCalculatorTest.java
+++ src/test/java/com/carector/StringCalculatorTest.java
@@ -7,8 +7,46 @@ public class StringCalculatorTest
 {
     
     @Test
-    public void shouldAnswerWithTrue()
+    public void testNegatives1()
     {
-        assertTrue( true );
+        try
+        {
+            int result = StringCalculator.add("-1, 5");
+            assert false;
+        }
+        catch(Exception e)
+        {
+            // If an exception was thrown, the test passed
+            assertTrue(e.getMessage().equals("Negatives not allowed."));
+        }
+    }
+
+    @Test
+    public void testNegatives2()
+    {
+        try
+        {
+            int result = StringCalculator.add("5, 4, 3, 2, 1, 0, -1, -2, -3, -4");
+            assert false;
+        }
+        catch(Exception e)
+        {
+            // If an exception was thrown, the test passed
+            assertTrue(e.getMessage().equals("Negatives not allowed."));
+        }
+    }
+
+    @Test
+    public void testGreaterThan1000_1()
+    {
+        int result = StringCalculator.add("1, 2, 3, 4, 99999");
+        assertTrue(result == 10);
+    }
+
+    @Test
+    public void testGreaterThan1000_2()
+    {
+        int result = StringCalculator.add("998, 999, 1000, 1001");
+        assertTrue(result == 1997);
     }
 }
